3629fe23982c36e0052a734f97d3a5e63320d3bc
Vladimir Sofronov <narviii@gmail.com>
2019-05-12 16:04:37 -0700

    initial

---
+++ app.js
@@ -0,0 +1,41 @@
+const Telegraf = require('telegraf')
+const dotenv = require('dotenv')
+dotenv.config();
+const geoTz = require('geo-tz')
+const { DateTime } = require("luxon");
+const commandParts = require('telegraf-command-parts');
+const googleMapsClient = require('@google/maps').createClient({
+    key: process.env.GOOGLE_TOKEN,
+    Promise: Promise
+  }); 
+  
+geoTz.preCache()
+dotenv.config();
+
+console.log(process.env.BOT_TOKEN)
+
+const bot = new Telegraf(process.env.BOT_TOKEN)
+bot.use(commandParts());
+
+bot.start((ctx) => {
+    //console.log(ctx.message)
+    ctx.reply('Welcome to Time Bot. Type /time command followed by place to get local time for that place. Try "/time tokyo"');
+    })
+
+bot.command('time', (ctx) => {
+    //console.log(typeof(ctx.state.command.args))
+    let response = googleMapsClient.geocode({address: ctx.state.command.args}).asPromise()
+    response.then((response) => {
+        tz = geoTz(response.json.results[0].geometry.location.lat, response.json.results[0].geometry.location.lng)
+        //console.log(tz)
+        locTime = DateTime.local().setZone(tz[0])
+        console.log(locTime.toLocaleString(DateTime.DATETIME_MED))
+        ctx.reply(`Local time in ${tz[0]} timezone is ${locTime.toLocaleString(DateTime.DATETIME_MED)}`)
+        
+    })
+    //console.log(ctx.state.command)
+    //ctx.reply('Hello')
+})
+
+bot.launch()
+
