f3ff2850161a4b098f663874355fde126bfc6206
Vladimir Sofronov <narviii@gmail.com>
2019-05-12 19:06:49 -0700

    ..

--- app.js
+++ app.js
@@ -22,17 +22,26 @@ bot.start((ctx) => {
     ctx.reply('Welcome to Time Bot. Type /time command followed by place to get local time for that place. Try "/time tokyo"');
     })
 
+bot.help((ctx) => {
+    ctx.reply('/time command followed by place to get local time for that place. Try "/time tokyo')
+})
+
 bot.command('time', (ctx) => {
     //console.log(typeof(ctx.state.command.args))
     let response = googleMapsClient.geocode({address: ctx.state.command.args}).asPromise()
     response.then((response) => {
         tz = geoTz(response.json.results[0].geometry.location.lat, response.json.results[0].geometry.location.lng)
         //console.log(tz)
+        //console.log(ctx.from)
         locTime = DateTime.local().setZone(tz[0])
-        console.log(locTime.toLocaleString(DateTime.DATETIME_MED))
+        
+        console.log(`${ctx.from.first_name} ${ctx.from.last_name} asked for time in ${tz[0]}: ${locTime.toLocaleString(DateTime.DATETIME_MED)}`)
         ctx.reply(`Local time in ${tz[0]} timezone is ${locTime.toLocaleString(DateTime.DATETIME_MED)}`)
         
     })
+        .catch(() => {
+            ctx.reply('Something gone wrong! Maybe wrong place?')
+        })
     //console.log(ctx.state.command)
     //ctx.reply('Hello')
 })
